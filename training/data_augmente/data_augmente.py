646703ade718e9292b70e2280a666e73daf75bb1
CamilleMaslin <65364408+MMillle@users.noreply.github.com>
2024-10-03 00:32:19 +0200

    Implem MobileNetV2 model + clean globale + doc

--- training/data_augmente/data_augmente.py
+++ training/data_augmente/data_augmente.py
@@ -1,3 +1,16 @@
+"""
+Brain Tumor Image Data Augmentation
+
+This script implements data augmentation techniques for brain tumor images.
+It loads images from a source directory, applies various augmentation
+techniques, and saves the augmented images to a new directory. This process
+helps to increase the diversity of the training dataset for improved model
+performance.
+
+Author: Camille Maslin
+Date:
+"""
+
 import tensorflow as tf
 from tensorflow.keras import layers
 import os
@@ -8,6 +21,7 @@ import numpy as np
 IMAGE_SIZE = 256
 AUGMENTATION_FACTOR = 2
 
+# Directory paths
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 data_dir = os.path.join(parent_dir, "data", "base_data")
@@ -21,18 +35,30 @@ data_augmentation = tf.keras.Sequential([
     layers.RandomContrast(0.2)
 ])
 
-# Fonction pour créer un dossier si nécessaire
 def create_dir(dir_path):
+    """
+    Create a directory if it doesn't exist.
+    
+    Args:
+        dir_path (str): Path of the directory to be created.
+    """
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
 
-# Créer un nouveau dataset avec des images augmentées
 def augment_and_save_images(dataset_dir, output_dir, augmentation_factor):
+    """
+    Augment images from the source directory and save them to the output directory.
+    
+    Args:
+        dataset_dir (str): Path to the source image directory.
+        output_dir (str): Path to the output directory for augmented images.
+        augmentation_factor (int): Number of augmented images to generate per original image.
+    """
     create_dir(output_dir)
 
     for subdir, dirs, files in os.walk(dataset_dir):
         for file in files:
-            if file.endswith(('.jpg', '.jpeg', '.png')):  # Assurez-vous que ce sont bien des fichiers images
+            if file.endswith(('.jpg', '.jpeg', '.png')):
                 image_path = os.path.join(subdir, file)
                 img = Image.open(image_path)
                 img = img.resize((IMAGE_SIZE, IMAGE_SIZE))
@@ -43,7 +69,7 @@ def augment_and_save_images(dataset_dir, output_dir, augmentation_factor):
                 
                 img_array = np.array(img)
 
-                # Vérifier si l'image est en niveaux de gris et la convertir en RGB si nécessaire
+                # Convert grayscale to RGB if necessary
                 if len(img_array.shape) == 2:
                     img_array = np.stack((img_array,)*3, axis=-1)
                 elif img_array.shape[2] == 1:
@@ -54,10 +80,10 @@ def augment_and_save_images(dataset_dir, output_dir, augmentation_factor):
                 label_dir = os.path.join(output_dir, os.path.basename(subdir))
                 create_dir(label_dir)
 
-                # Sauvegarder l'image originale
+                # Save the original image
                 Image.fromarray(img_array).save(os.path.join(label_dir, file))
 
-                # Appliquer les augmentations et sauvegarder les images modifiées
+                # Apply augmentations and save modified images
                 for i in range(augmentation_factor):
                     augmented_img = data_augmentation(tf.expand_dims(img_array, 0), training=True)
                     augmented_img = tf.squeeze(augmented_img).numpy().astype("uint8")
@@ -67,10 +93,10 @@ def augment_and_save_images(dataset_dir, output_dir, augmentation_factor):
                     
                     augmented_img_pil = Image.fromarray(augmented_img)
 
-                    # Sauvegarder l'image augmentée avec un nouveau nom
+                    # Save the augmented image with a new name
                     augmented_img_pil.save(os.path.join(label_dir, f"aug_{i}_{file}"))
 
-# Appeler la fonction pour générer des images augmentées
+# Generate augmented images
 augment_and_save_images(data_dir, output_dir, AUGMENTATION_FACTOR)
 
-print(f"Les images augmentées ont été sauvegardées dans : {output_dir}")
+print(f"Augmented images have been saved in: {output_dir}")
